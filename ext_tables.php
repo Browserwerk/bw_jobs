096ce3934aacfc1c890d5d88777c0a8e59044953
Vincent Bärtsch <v.baertsch@browserwerk.de>
2020-08-25 15:08:22 +0200

    Remove typo3 use namespaces

--- ext_tables.php
+++ ext_tables.php
@@ -1,14 +1,11 @@
 <?php
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
-
 defined('TYPO3_MODE') || die('Access denied.');
 
 call_user_func(
     function () {
         if (TYPO3_MODE === 'BE') {
-            ExtensionUtility::registerModule(
+            TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
                 'Browserwerk.BwJobs',
                 'web', // Make module a submodule of 'web'
                 'bwjobs', // Submodule key
@@ -26,39 +23,38 @@ call_user_func(
                 ]
             );
         }
-
         // add static includes
-        ExtensionManagementUtility::addStaticFile('bw_jobs', 'Configuration/TypoScript', 'Bw_Jobs');
+        TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('bw_jobs', 'Configuration/TypoScript', 'Bw_Jobs');
 
         // job language config
-        ExtensionManagementUtility::addLLrefForTCAdescr(
+        TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
             'tx_bwjobs_domain_model_job',
             'EXT:bw_jobs/Resources/Private/Language/locallang_csh_tx_bwjobs_domain_model_job.xlf'
         );
-        ExtensionManagementUtility::allowTableOnStandardPages('tx_bwjobs_domain_model_job');
+        TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_bwjobs_domain_model_job');
 
         // contact language config
-        ExtensionManagementUtility::addLLrefForTCAdescr(
+        TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
             'tx_bwjobs_domain_model_contact',
             'EXT:bw_jobs/Resources/Private/Language/locallang_csh_tx_bwjobs_domain_model_contact.xlf'
         );
-        ExtensionManagementUtility::allowTableOnStandardPages('tx_bwjobs_domain_model_contact');
+        TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_bwjobs_domain_model_contact');
 
         // job language config
-        ExtensionManagementUtility::addLLrefForTCAdescr(
+        TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
             'tx_bwjobs_domain_model_location',
             'EXT:bw_jobs/Resources/Private/Language/locallang_csh_tx_bwjobs_domain_model_location.xlf'
         );
-        ExtensionManagementUtility::allowTableOnStandardPages('tx_bwjobs_domain_model_location');
+        TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_bwjobs_domain_model_location');
 
         // jobType language config
-        ExtensionManagementUtility::addLLrefForTCAdescr(
+        TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
             'tx_bwjobs_domain_model_jobtype',
             'EXT:bw_jobs/Resources/Private/Language/locallang_csh_tx_bwjobs_domain_model_jobtype.xlf'
         );
-        ExtensionManagementUtility::allowTableOnStandardPages('tx_bwjobs_domain_model_jobtype');
+        TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_bwjobs_domain_model_jobtype');
 
-        ExtensionUtility::registerPlugin(
+        TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
             'Browserwerk.BwJobs',
             'Frontend',
             'Frontend'
